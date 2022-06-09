3f07d650865700805055ecb4f85aed49f284ab50
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-06-09 16:45:30 -0500

    made kirby move

--- js/v-customobjects.js
+++ js/v-customobjects.js
@@ -23,6 +23,7 @@ Vue.component("obj-head", {
         gltf-model="url(models/kirby/scene.gltf)"
         scale="0.2 0.2 0.2"
         rotation="0 180 0"
+        position="0 -1.5 0"
       ></a-entity>
 	</a-entity>
   <a-entity v-else>
@@ -95,6 +96,9 @@ Vue.component("obj-head", {
     },
     headSize() {
       return this.obj.size instanceof Vector ? this.obj.size.x : this.obj.size;
+    },
+    fake() {
+      return this.obj.uid.slice(0, 5) === "FAKE_";
     },
   },
 
@@ -409,7 +413,7 @@ Vue.component("obj-world", {
 			rotation = "0 270 0">
       scale="1.5 1.5 1.5">
 		</a-entity>
-
+  <!---
 		<a-entity gltf-model="url(models/kirby/scene.gltf)" 
 			position="-5 0 -6"
 			rotation = "0 30 0"
