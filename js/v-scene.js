2988e265f3fa68230cadf620cf55cd8e799754a8
Yvan Chu <60163603+yvanchu@users.noreply.github.com>
2022-06-08 22:26:49 -0500

    removed debug text

--- js/v-scene.js
+++ js/v-scene.js
@@ -1,38 +1,40 @@
-let worldCamera = undefined
+let worldCamera = undefined;
 
-AFRAME.registerComponent('rotation-reader', {
-	// Track the camera's position 
-	// and copy it into the user's head
+AFRAME.registerComponent("rotation-reader", {
+  // Track the camera's position
+  // and copy it into the user's head
 
   /**
    * We use IIFE (immediately-invoked function expression) to only allocate one
    * vector or euler and not re-create on every tick to save memory.
    */
-	init: function () {
-		console.log('Begin tracking the camera');
-		worldCamera = this.el
-		console.log("CAMERA:", worldCamera)
-		let r = Math.random()*1 + 1
-		let theta = 100*Math.random()*1 + 1
-		worldCamera.object3D.position.set(r*Math.cos(theta), 1.5, r*Math.sin(theta))
-		worldCamera.object3D.lookAt(0,.5,0)
-		console.log("****Start pos", worldCamera.object3D.position.toArray())
-	},
-	tick: (function () {
-		
-		return function () {
-			if (room.userHead) {
-				room.userHead.position.copy(this.el.object3D.position)
-				room.userHead.rotation.copy(this.el.object3D.rotation)
-				room.userHead.post()
-			}
-
-		};
-	})()
+  init: function () {
+    console.log("Begin tracking the camera");
+    worldCamera = this.el;
+    console.log("CAMERA:", worldCamera);
+    let r = Math.random() * 1 + 1;
+    let theta = 100 * Math.random() * 1 + 1;
+    worldCamera.object3D.position.set(
+      r * Math.cos(theta),
+      1.5,
+      r * Math.sin(theta)
+    );
+    worldCamera.object3D.lookAt(0, 0.5, 0);
+    console.log("****Start pos", worldCamera.object3D.position.toArray());
+  },
+  tick: (function () {
+    return function () {
+      if (room.userHead) {
+        room.userHead.position.copy(this.el.object3D.position);
+        room.userHead.rotation.copy(this.el.object3D.rotation);
+        room.userHead.post();
+      }
+    };
+  })(),
 });
 
 Vue.component("room-scene", {
-	template: `<a-scene>
+  template: `<a-scene>
 
 		
 		<!--------- ASSETS ---------->
@@ -46,6 +48,7 @@ Vue.component("room-scene", {
 			<a-cursor></a-cursor>
 
 			<!-------- Output text ----->
+			<!--------
 			<a-entity>
 				<a-text 
 					v-if="room.userHead"
@@ -68,6 +71,7 @@ Vue.component("room-scene", {
 					position="-.7 .5 -1">
 				</a-text>
 			</a-entity>
+			----->
 			
 		</a-camera>
 		
@@ -86,20 +90,18 @@ Vue.component("room-scene", {
 
 	</a-scene>`,
 
-	methods: {
-		camtick() {
-			console.log("cam")
-		}
-	},
-	mounted() {
-		// Create 
-	},
+  methods: {
+    camtick() {
+      console.log("cam");
+    },
+  },
+  mounted() {
+    // Create
+  },
 
-	data() {
-		return  {
-			
-		}
-	},
+  data() {
+    return {};
+  },
 
-	props: ["room"],
-})
+  props: ["room"],
+});
